f216fe49903f2ec4f322e45effbb82e8de2c8808
MatveyPreo <98408517+MatveyPreo@users.noreply.github.com>
2022-02-01 19:47:37 +0200

    Buttons, Image Modalst

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, SafeAreaView, Button } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, Button, Alert, Image } from 'react-native';
 
 export default function App() {
   const TextPress = () => console.log('Text Pressed');
-
+  const ButtonPress2 = () => Alert.prompt("write", "что угодно", text2 => console.log(text2));
+  const ButtonPress = () => Alert.alert("error", "404", [
+    { text: "ОК", onPress: () => console.log('Yes button') },
+    { text: "ОТМЕНА", onPress: () => console.log('No button') }
+  ]);
+  
+  // ghgfhfgh
   return (
     <SafeAreaView style={styles.container}>
-      <Text numberOfLines={2} style={styles.text} onPress={TextPress}>Hi {'\n'}hi </Text>
+
+      <Text numberOfLines={2} style={styles.text} >Hi {'\n'}hi </Text>
+      <Button title={'нажми меня'} color={'red'} onPress={ButtonPress} />
+      <Image source={{
+        width: 200,
+        height: 150,
+        uri: "https://static5.depositphotos.com/1007168/472/i/950/depositphotos_4725473-stock-photo-hot-summer-sun-wearing-shades.jpg"
+      }}/>
+      <Button title={'впиши что-то'} color={'red'} onPress={ButtonPress2} />
+      {/* комментарии  */}
+      {/*<Image source={require('./assets/adaptive-icon.png')} />*/}
+      
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -20,6 +37,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   text: {
-    color: 'red'   
+    color: 'red'
   },
 });
