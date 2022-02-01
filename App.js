7d4c9286232fdeaf852f54f5a5103bbee6d7a56b
MatveyPreo <98408517+MatveyPreo@users.noreply.github.com>
2022-02-01 21:04:28 +0200

    style, chek platform, Positioning objects

--- App.js
+++ App.js
@@ -1,42 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, SafeAreaView, Button, Alert, Image } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, View, Platform } from 'react-native';
 
 export default function App() {
-  const TextPress = () => console.log('Text Pressed');
-  const ButtonPress2 = () => Alert.prompt("write", "что угодно", text2 => console.log(text2));
-  const ButtonPress = () => Alert.alert("error", "404", [
-    { text: "ОК", onPress: () => console.log('Yes button') },
-    { text: "ОТМЕНА", onPress: () => console.log('No button') }
-  ]);
   
   // ghgfhfgh
   return (
     <SafeAreaView style={styles.container}>
-
-      <Text numberOfLines={2} style={styles.text} >Hi {'\n'}hi </Text>
-      <Button title={'нажми меня'} color={'red'} onPress={ButtonPress} />
-      <Image source={{
-        width: 200,
-        height: 150,
-        uri: "https://static5.depositphotos.com/1007168/472/i/950/depositphotos_4725473-stock-photo-hot-summer-sun-wearing-shades.jpg"
-      }}/>
-      <Button title={'впиши что-то'} color={'red'} onPress={ButtonPress2} />
-      {/* комментарии  */}
-      {/*<Image source={require('./assets/adaptive-icon.png')} />*/}
+      <View style={styles.box}>
+      <Text>Hello </Text>
+      </View>
       
       <StatusBar style="auto" />
     </SafeAreaView>
   );
 }
-
+const simpleStyle = {backgroundColor: 'blue', color: 'red'}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  
   },
-  text: {
-    color: 'red'
-  },
+  box: {
+    backgroundColor: 'green',
+    width: '50%',
+    height: '50%',
+    opacity: 0.9, //прозрачность
+    borderWidth: 2, // обводка
+    borderColor: 'red',
+    top: Platform.OS === "ios" ? 150 : 160,
+    left: 150
+  }
 });
