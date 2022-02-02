0b113b95cde83ae56328d01de63e291aa33636a1
MatveyPreo <98408517+MatveyPreo@users.noreply.github.com>
2022-02-02 19:53:31 +0200

    ToDos

--- App.js
+++ App.js
@@ -1,31 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, SafeAreaView, View } from 'react-native';
-import { backgroundColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
+import React from 'react'
+import { StyleSheet, View } from 'react-native-web'
+import Header from './components/header';
 
 export default function App() {
-  
-  // ghgfhfgh
   return (
-    <SafeAreaView style={styles.mainBlock}>
-      <View style={[styles.box, {backgroundColor: 'yellow'}]}></View>
-      <View style={[styles.box, {backgroundColor: 'red'}]}></View>
-      <View style={[styles.box, {backgroundColor: 'blue'}]}></View>
-    </SafeAreaView>
+    <View>
+      <Header />
+    </ View >
   );
 }
-const simpleStyle = {backgroundColor: 'blue', color: 'red'}
+
 const styles = StyleSheet.create({
-  mainBlock: {
-    flex: 1,
-    flexDirection: 'column', //в колонку
-    //flexDirection: 'column-reverse'- колонка в обратном порядке
-    //flexDirection: 'row' - поочереди 
-    justifyContent: 'space-around',
-    alignItems: 'center'
-  },
-  box:{
-    height: 100,
-    width: 100
-  }
 
 });
---
+++ components/header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Header() {
+    return (
+        <View>
+            <Text>Список дел</Text>
+        </View>
+    );
+}
+
+
+
+// export default function Header() {
+//     return (
+//         <View style={styles.main}>
+//             <Text style={styles.text}>Список дел</Text>
+//         </View>
+//     )
+// }
+
+
+// const styles = StyleSheet.create({
+//     main: {
+//         paddingTop: 100,
+//         height: 100,
+//         backgroundColor: 'silver'
+//     },
+//     text: {
+//         fontSize: 18,
+//         color: 'red'
+//     }
+// });
