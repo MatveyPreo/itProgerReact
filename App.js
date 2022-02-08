93775b7c50276c46a7fd3e1340c45bbb59d0d3a5
MatveyPreo <98408517+MatveyPreo@users.noreply.github.com>
2022-02-08 17:59:32 +0200

    to-do list

--- App.js
+++ App.js
@@ -1,15 +1,42 @@
-import React from 'react'
-import { StyleSheet, View } from 'react-native-web'
-import Header from './components/header';
-
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import Header from './components/Header'
+import ListItem from './components/ListItem'
+import Form from './components/Form'
 export default function App() {
+  const [listOfItems, setlistOfItems] = useState([
+    {text: 'купить молоко', key: '1'},
+    {text: 'помыть машину', key: '2'},
+    {text: 'купить картошку', key: '3'},
+    {text: 'убратть в комнате', key: '4'}
+  ])
+  const addHandler = (text) =>{
+    setlistOfItems((list) => {
+      return[
+        { text: text, key: Math.random().toString(36).substring(7)},
+        ...list
+      ]
+    })
+  }
+  const deleteHandler = (key) => {
+    setlistOfItems((list) => {
+      return list.filter(listOfItems => listOfItems.key != key)
+    });
+  }
   return (
-    <View>
-      <Header />
-    </ View >
+    <View >
+      <Header/>
+      <Form addHandler={addHandler} />
+      <View>
+        <FlatList data ={listOfItems} renderItem={({ item}) => (
+          <ListItem el={item} deleteHandler={deleteHandler} />
+        )}/>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
 
 });
+
---
+++ components/Form.js
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+import { StyleSheet, View, Text, TextInput, Button } from 'react-native';
+
+export default function Form({addHandler}) {
+    const [text, setValue] = useState('');
+    const onChange = (text) => {
+        setValue(text);
+    };
+    return (
+        <View>
+            <TextInput style={ styles.input } onChangeText={onChange} placeholder='Новое дело' />
+            <Button color ='blue' onPress={() => addHandler(text)} title='новое дело'/>
+        </View>
+ 
+    );
+}
+
+const styles = StyleSheet.create({
+    input: {
+        borderBottomWidth: 1,
+        borderColor: 'black',
+        padding: 10,
+        marginVertical: 30,
+        marginHorizontal: '20%',
+        width: '60%'
+    }
+});
